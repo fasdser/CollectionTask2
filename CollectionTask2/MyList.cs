b6aa89b39b0ad62487e25399efe3f2e1bc97f87e
fasdser <107477645+fasdser@users.noreply.github.com>
2022-08-02 14:13:44 +0300

    Add project files.

---
+++ CollectionTask2/MyList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CollectionTask2
+{
+    public interface IMyList<T>
+    {
+        void Add(T item);
+        T this[int index] { get; }
+        int Count { get; }
+        void Clear();
+        bool Contains(T item);
+    }
+
+    class MyList<T>:IMyList<T>
+    {
+        private T[] items;
+
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] array = new T[items.Length+1];
+            for (int i = 0; i < items.Length; i++)
+            {
+                array[i] = items[i];
+            }
+            array[items.Length] = item;
+            items = array;
+        }
+
+        public T this[int index]
+        {
+            get { return items[index]; }
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+        public void Clear()
+        {
+            items = new T[0];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if ((int)(object)items[i] == (int)(object)item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = string.Empty;
+            for (int i = 0; i < items.Length; i++)
+            {
+                str += items[i] + " ";
+            }
+            return "Размерность массива " + items.Length + " Элемент массива:" + str; 
+        }
+
+        int position = -1;
+
+        public void Reset()
+        {
+            position = -1;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < items.Length - 1)
+                {
+                    position++;
+                    yield return items[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+        }
+    }
+}
---
+++ CollectionTask2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> list = new MyList<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+            list.Add("d");
+            list.Add("e");
+            list.Add("f");
+            list.Add("g");
+            list.Add("h");
+            list.Add("i");
+                
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            Console.WriteLine(list.Count);
+           
+           
+            Console.ReadLine();
+        }
+    }
+}
